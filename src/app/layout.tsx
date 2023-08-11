79b10264d888ed15328c85001b1b0fdefe97cac2
LineaLauncher Senior <senior.dev@linealauncher.com>
2023-08-12 02:37:53 +0300

    rename projects folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import type { Metadata } from "next"
 import Navbar from "@/components/Navbar"
 import Footer from "@/components/Footer"
 import clsx from "clsx"
-import EthereumProvider from "@/Providers/EthereumProvider"
+import EthereumProvider from "@/providers/EthereumProvider"
 
 const robotoMono = Roboto_Mono({ subsets: ["latin"] })
 
